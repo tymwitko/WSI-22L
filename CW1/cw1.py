277f43c675c57622c5a1e837e8d994b6b042acdf
tymwitko <tymon.kobylecki@gmail.com>
2022-03-10 20:20:02 +0100

    dokumentacja i refactoring

--- CW1/cw1.py
+++ CW1/cw1.py
@@ -1,3 +1,5 @@
+# Tymon Kobylecki WSI 22L
+
 import math
 
 def f(x):
@@ -11,10 +13,10 @@ def g(x1, x2):
 
 def dg(x1, x2):
     d1 = 2 * x1 * math.exp(-x1**2 - x2**2) + (x1 - 1) * math.exp(-(x1 - 1)**2 - (x2 + 2)**2)
-    d2 = 2 * x2 * math.exp(-x1**2 - x2**2) + (x2 - 1) * math.exp(-(x1 - 1)**2 - (x2 + 2)**2)
+    d2 = 2 * x2 * math.exp(-x1**2 - x2**2) + (x2 + 2) * math.exp(-(x1 - 1)**2 - (x2 + 2)**2)
     return (d1, d2)
 
-if __name__ == "__main__":
+def collect_inputs():
     print("Welcome! Select the function (f or g):")
     func = input()
     print("Enter initial point x1:")
@@ -26,29 +28,46 @@ if __name__ == "__main__":
         x2 = 0
     print ("Enter the beta parameter:")
     beta = float(input())
-    run = True
     print("Enter the desired tolerance")
     tol = float(input())
-    while run:
+    return func, x1, x2, beta, tol
+
+def gradient(func, x1, beta, x2 = 0):
+    if func == "f":
+        d1 = df(x1)
+        x1 -= beta * d1
+        print("x = " + str(x1))
+        return x1, d1
+    elif func == "g":
+        d1 = dg(x1, x2)[0]
+        d2 = dg(x1, x2)[1]
+        x1 -= beta * d1
+        x2 -= beta * d2
+        print("x1 = " + str(x1) + ", x2 = " + str(x2) + ", y = " + str(g(x1, x2)) + ", dy = " + str(dg(x1, x2)))
+        return x1, d1, x2, d2
+    
+
+if __name__ == "__main__":
+    (func, x1, x2, beta, tol) = collect_inputs()
+    print(dg(1, -2))
+    steps = 0
+    while True:
+        steps += 1
         try:
             if func == "f":
-                d1 = df(x1)
+                (x1, d1) = gradient(func, x1, beta)
             elif func == "g":
-                d1 = dg(x1, x2)[0]
-                print(d1, dg(x1, x2))
-                d2 = dg(x1, x2)[1]
-                x2 -= beta * d2
-            x1 -= beta * d1
-            print(x1, x2)
+                (x1, d1, x2, d2) = gradient(func, x1, beta, x2)
             if abs(d1) <= tol:
                 if func == "f":
-                    print ("Minimum found!")
+                    print ("Minimum found in " + str(steps) + " steps")
                     print("x = " + str(x1) + "\ny = " + str(f(x1)))
                     break
                 elif func == "g":
                     if abs(d2) <= tol:
+                        print ("Minimum found in " + str(steps) + " steps")
                         print("x1 = " + str(x1) + "\nx2 = " + str(x2) + "\ny = " + str(g(x2, x2)))
                         break
         except OverflowError:
             print ("Overflow Error! Decrease the beta parameter or change the initial point.")
-            run = False
+            break
