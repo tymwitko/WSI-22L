8677713cf055840f7d50d0d83daec429175aefce
tymwitko <62263510+tymwitko@users.noreply.github.com>
2022-03-25 12:03:00 +0100

    Update cw2.py
    Objąć te zera

--- CW2/cw2.py
+++ CW2/cw2.py
@@ -1,3 +1,9 @@
+from cmath import inf
+import random
+
+import numpy as np
+
+
 class NoFuelError(Exception):
     pass
 
@@ -5,7 +11,7 @@ class EngineOffError(Exception):
     pass
 
 def goal_func(h, fuel):
-    if h >= 750:
+    if h >= 150: #750:
         return 200 - fuel
     else:
         return 0
@@ -27,6 +33,9 @@ def d_height(fuel, engine):
     a_sum = acc_up(fuel, engine) + gravity_down()
     v = a_sum * 1
     a_sum += fric_down(v, 20 + fuel)
+    v = a_sum * 1
+    return v * 1
+    
 
 def height(pattern):
     fuel = sum(pattern)
@@ -52,7 +61,7 @@ def height(pattern):
 #         Pt + 1 = Ot
 #         t = t+1
 
-def genetic(q, P, μ, pm, pc, tmax):
+def genetic(q, μ, pm, pc, iters, length):
     '''
     q - funkcja celu
     P - populacja poczatkowa
@@ -61,19 +70,22 @@ def genetic(q, P, μ, pm, pc, tmax):
     pc - prawdopodobienstwo krzyzowania
     '''
     t = 0
-    orate = rate( q, P )
-    while True:
-        R = select(P, orate, μ )
-        M = cross_mut(R, pm, pc )
-        orate = rate( q, M )
-        P = M
+    Po = popul_init(μ, length)
+    orate = rate( q, Po )
+    while t < iters:
+        Re = select(Po, orate, μ )
+        Mu = cross_mut(Re, pm, pc )
+        orate = rate( q, Mu )
+        Po = Mu
         t = t + 1
-        # dodac warunek stopu!
-    return P, orate
+    return Po, orate, [height(i) for i in Po]
 
-def probability(P, unit):
+def probability(P, pattern):
     if pattern in P:
-        return height(unit)/sum_goal(P)
+        try:
+            return height(pattern)/sum_goal(P)
+        except ZeroDivisionError:
+            return 0
 
 def sum_goal(P):
     summ = 0
@@ -83,25 +95,51 @@ def sum_goal(P):
 
 def select(P, o, μ):
     weights = []
-    for i in range(len(P)):
-        weights.append(probability(P, P[i]))
-    return random.choices(P, weights, None, len(P)-1) # TODO: jak dobierać liczebność populacji po selekcji (tu len(P)-1)? Czy to element zadania?
+    sel_inds = []
+    for i in range(μ):
+        weights.append(probability(P, P[i])) # po pewnym czasie same zera
+    for i in range(μ):
+        sel_inds.append(random.choices(range(0,μ), weights=weights, k=1)[0])
+    print(sel_inds)
+    print(weights)
+    try:
+        return [P[i] for i in sel_inds] # liczebność populacji stała, mogą być duplikaty
+    except IndexError:
+        print("AAAAAAAAAA! " + str(len(P)), len(sel_inds))
+        print(P[99])
 
 def rate(q, P):
     tab = []
     for i in P:
-        tab.append(q(i))
+        tab.append(q(height(i),sum(i)))
     return tab
 
 def cross_mut(R, pm, pc):
     temp = cross(R, pc)
     temp = mut(temp, pm)
+    return temp
 
 def cross(P, pc):
-    for pattern in P:
+    ind = 0
+    crossed = []
+    while ind < len(P):
         if random.random() <= pc:
-            point = random.randrange(len(pattern))
-            temp = pattern[point:] #TODO: jak dobierać drugi? czy mogą się powtarzać?
+            try:
+                point = random.randrange(len(P[ind]))
+                temp = P[ind][point:] #jak jest npar to po prostu dobrać jakiegoś powtórzonego i zostawić 1 dziecko
+                P[ind][point:] = P[ind+1][point:]
+                P[ind+1][point:] = temp
+                crossed.append(P[ind])
+                crossed.append(P[ind+1])
+            except IndexError:
+                point = random.randrange(len(P[ind]))
+                P[ind][point:] = P[0][point:]
+                crossed.append(P[ind][point:])
+        else:
+            crossed.append(P[ind])
+            crossed.append(P[ind+1])
+        ind += 2
+    return crossed
 
 def mut(P, pm):
     for pattern in P:
@@ -113,7 +151,19 @@ def mut(P, pm):
                     unit = 1
     return P
 
+def popul_init(μ, length):
+    Po = []
+    for _ in range(μ):
+        temp = np.random.choice([0, 1], size=(length,))
+        Po.append(temp.tolist())
+    return Po
+
 if __name__ == "__main__":
-    tiem = 200
+    tiem = 10 #200
     h_init = 0
     goal = 750
+    iters = 100
+    pop_size = 20
+    pm = 0.3
+    pc = 0.2
+    print(genetic(goal_func, pop_size, pm, pc, iters, tiem))
